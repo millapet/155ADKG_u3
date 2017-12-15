67919990f3092bd8c7162c096664b3b9b89a62f5
Petra Millarova <petramillarova@gmail.com>
2017-12-15 19:58:25 +0100

    Initial commit

---
+++ DTM/algorithms.h
@@ -0,0 +1,23 @@
+#ifndef ALGORITHMS_H
+#define ALGORITHMS_H
+#include <vector>
+
+#include "edge.h"
+#include "qpoint3d.h"
+
+class Algorithms
+{
+public:
+    Algorithms();
+    static int getPosition(QPoint3D &q,QPoint3D &a, QPoint3D &b);
+    static double getCircleRadius(QPoint3D &p1, QPoint3D &p2,QPoint3D &p3);
+    static int getDelaunayPoint(Edge &e, std::vector <QPoint3D> &points);
+    static int getNearestPoint(QPoint3D &p, std::vector <QPoint3D> &points);
+
+    static std::vector<Edge> dt(std::vector <QPoint3D> &points);
+    static QPoint3D getConPoint(QPoint3D &p1,QPoint3D &p2, double z);
+
+    static std::vector<Edge> createContours(std::vector<Edge>&dt, double zmin, double zmax, double h);
+};
+
+#endif // ALGORITHMS_H
